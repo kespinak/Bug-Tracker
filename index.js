256f573fb048dfdef7f1164a1da05c0a207ee637
kespinak <kespinak@gmail.com>
2022-12-01 09:37:19 -0800

    REDID THE WHOLE FILES...i added create-react-app, redux, react-redux, material-ui /core & /icons

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '.components/App';
-import* as swerviceWorker from './serviceWorker';
+import App from '../../components/App';
+// import App from './components/App'; 5:10min
+// import* as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, {Component } from 'react';
+import './App.css';
+import React, { Component } from 'react';
 
-class App extends Component {
-    render() {
-        return (
-            <div className='App'>
-                <h2>Hello bug-tracker Application!</h2>
-            </div>
-        )
-    }
+// class App extends Component { // 5:30min
+function App() {
+  return (
+    <React.Fragment>
+      <div className='App'>
+        <h2>Hello from bug-tracker\src\App.js in h2-brackets</h2>
+      </div>
+      <h3>Hello from bug-tracker\src\App.js in p-brackets</h3>
+    </React.Fragment>
+  );
 }
+
+export default App;
