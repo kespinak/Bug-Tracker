3b7bf5d1a6e312ed75094f762652c2a9297b6541
kespinak <kespinak@gmail.com>
2022-11-30 22:09:14 -0800

    added 4 node modules (redux, react-redux, material-ui core, material-ui icons)

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <h2>Hello bug-tracker Application!</h2>
+            </div>
+        )
+    }
+}
